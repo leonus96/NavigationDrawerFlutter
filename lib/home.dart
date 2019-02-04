140b987fe688f502c4a0c72c4a67234453724891
Joseph León <leonus96@gmail.com>
2019-02-04 12:30:44 -0500

    primera parte

---
+++ lib/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Navigation Drawer'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: (){
+
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: (){
+                
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
